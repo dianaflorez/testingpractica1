c2e8a3c1940b5982421cd6478505c7fe245a1561
Diana <dianaflorezbravo@gmail.com>
2024-07-16 15:13:26 -0500

    Initial commit

---
+++ src/05-setup.test.js
@@ -0,0 +1,16 @@
+describe('Set', () => {
+  test('case 1', () => {
+    console.log('case 1');
+    expect(1 + 1).toBe(2);
+  });
+
+  test('case 2', () => {
+    console.log('case 2');
+    expect(1 + 1).toBe(2);
+  });
+
+  test('case 3', () => {
+    console.log('case 3');
+    expect(1 + 1).toBe(2);
+  });
+})
